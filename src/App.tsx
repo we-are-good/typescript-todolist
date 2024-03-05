4dcf15810f5461b957b0bec0bae0cfdc9b49192d
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-03-05 15:14:43 +0900

    Setup: program setting

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TodoForm />
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
