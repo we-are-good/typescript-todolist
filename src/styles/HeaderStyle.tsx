cfe68b15c410207db6ed9c285c534aafd190e736
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-03-05 15:50:00 +0900

    Style: edited header style'

---
+++ src/styles/HeaderStyle.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  height: 2rem;
+  background-color: greenyellow;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 40px;
+  font-weight: 800;
+`;
