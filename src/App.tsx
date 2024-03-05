f60681c1540c27b7fd5028910398f889abb7f1e4
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-03-05 15:42:21 +0900

    Style:GlobalStyle setting

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import "./App.css";
 import Header from "./components/Header";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <TodoForm />
       <TodoList />
