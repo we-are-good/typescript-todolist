782d4456620cae4bed224131364f89e45e88d7e3
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-03-05 21:39:53 +0900

    Feat: TodoList read addition

--- src/styles/TodoFormStyle.tsx
+++ src/styles/TodoFormStyle.tsx
@@ -5,22 +5,39 @@ export const TodoTitleWrapper = styled.article`
   height: 16rem;
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 1.5rem;
   background-color: #edfdcd;
   border: 5px solid green;
   border-radius: 2rem;
   margin: 2rem 1rem;
   padding: 2rem;
+`;
 
-  & header {
-    font-size: 1.5rem;
-  }
+export const TodoTitleInput = styled.input`
+  font-size: 1.2rem;
+  text-align: center;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 export const InputDataWrapper = styled.main`
+  height: 10rem;
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 0.8rem;
+`;
+
+export const TodoContentTextarea = styled.textarea`
+  height: 5rem;
+  font-size: 1rem;
+  text-align: center;
+  border: none;
+  background-color: transparent;
+  resize: none;
+  cursor: pointer;
 `;
 
 export const ButtonWrapper = styled.section`
@@ -29,4 +46,10 @@ export const ButtonWrapper = styled.section`
   align-items: center;
   justify-content: center;
   gap: 2rem;
+  & button {
+    width: 4rem;
+    height: 1%.5;
+    background-color: #beffaf;
+    border-radius: 8px;
+  }
 `;
